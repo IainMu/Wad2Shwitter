99eeded7c10a079f80ddf83fd84fdc1e7a0ad3f5
Corey Ansley <2266643A@gla.ac.uk>
2018-03-16 16:17:04 +0000

    starting on template tags

--- Schwitter_Project/Schwitter/templatetags/schwitter_template_tags.py
+++ Schwitter_Project/Schwitter/templatetags/schwitter_template_tags.py
@@ -1,5 +1,10 @@
 from django import template
+from Schwitter.models import Post
 
 register = template.Library()
+
+@register.inclusion_tag('schwitter/' )
+def get_post_list():
+    return {'posts': Post.objects.all()}
 
 
