2161becff457190be01cf93c9de24e5c2a0e588e
IainMu <35377396+IainMu@users.noreply.github.com>
2018-03-16 12:08:31 +0000

    Add files via upload

---
+++ Schwitter_Project/Schwitter/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.models import User
+from Schwitter.models import Category, Page, UserProfile
+
+class UserForm(forms.ModelForm):
+    password=forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model=User
+        fields = ('username','email','password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model=UserProfile
+        fields=('picture')
+
+class Post_form(forms.ModelForm):
+    likes = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
+    poster= forms.ForeignKey(widget=forms.HiddenInput(),initial=None)
+    time=forms.DateTimeField(widget=forms.HiddenInput(),initial=None)
+    class Meta:
+        model=Post
+        fields(title,content)
+
+class Comment_form(forms.ModelFrom):
+    likes = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
+    time=forms.DateTimeField(widget=forms.HiddenInput(),initial=None)
+    class Meta:
+        model=Comment
+        fields(content)
