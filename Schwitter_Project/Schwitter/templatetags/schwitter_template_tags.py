2641e32e71f6a8987aa78edf26ce832b0aecad5e
IainMu <35377396+IainMu@users.noreply.github.com>
2018-03-16 15:49:53 +0000

    Friday End
    Started template tag implementation, will continue Sunday

--- Schwitter_Project/Schwitter/templatetags/schwitter_template_tags.py
+++ Schwitter_Project/Schwitter/templatetags/schwitter_template_tags.py
@@ -1,5 +1,12 @@
 from django import template
+from schwitter.models import Post,Comment
 
 register = template.Library()
 
+@register.inclusion_tag('schwitter/posts.html')
+def get_post_list(poster=None):
+    return('posts':Post.objects.filter(poster=poster))
 
+@register.inclusion_tag('schwitter/comments.html')
+def get_comment_list(post=None):
+    return('comments':Comment.objects.filter(post=post))
