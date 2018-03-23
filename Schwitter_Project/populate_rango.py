b5dab9f0ab28563a87f50fdc2847c2b4c9928862
Corey Ansley <2266643A@gla.ac.uk>
2018-03-23 15:43:05 +0000

    fixed populate script

--- Schwitter_Project/populate_rango.py
+++ Schwitter_Project/populate_rango.py
@@ -67,12 +67,20 @@ def populate():
 
     for user in users:
         up = add_userprofile(user)
-        for post in Posts:
-            if post["poster"] == user["username"]:
-                p = add_post(post)
-            for comment in Comments:
-                if comment["post"] == post["slug"]:
-                    c = add_comment(comment)
+        print("added " + user["username"])
+
+
+
+    for user in users:
+        add_friends(user)
+
+    for post in Posts:
+        p = add_post(post)
+        print("added " + post["title"])
+        for comment in Comments:
+            if comment["post"] == post["slug"]:
+                add_comment(comment, p)
+                print("added " + comment["poster"] + "'s comment on " + comment["post"])
 
 def add_user(user):
     u = User.objects.get_or_create(username = user["username"], email = user["email"], password = user["password"])[0]
@@ -80,17 +88,26 @@ def add_user(user):
     return u
 
 def add_userprofile(user):
-    u = UserProfile.objects.get_or_create(user = add_user(user),friends=user["friends"])[0]
+    u = UserProfile.objects.get_or_create(user = add_user(user))[0]
     u.save()
     return u
+def add_friends(user):
+    u = UserProfile.objects.get(user = User.objects.get(username = user["username"]))
+    for friend in user["friends"]:
+        f = UserProfile.objects.get(user=User.objects.get(username=friend))
+        u.friends.add(f)
+    u.save()
 
 def add_post(post):
-    p = Post.objects.get_or_create(poster = post["poster"], title = post["title"], time = post["time"] , content = post["content"], likes = post["likes"], slug = post["slug"] )[0]
+    u = UserProfile.objects.get(user=User.objects.get(username=post["poster"]))
+    p = Post.objects.get_or_create(poster = u, title = post["title"], time = post["time"] , content = post["content"], likes = post["likes"], slug = post["slug"] )[0]
     p.save()
     return p
 
-def add_comment(comment):
-    c = Comment.objects.get_or_create(poster= comment["poster"], post=comment["post"], time=comment["time"], content=comment["content"], likes=comment["likes"])[0]
+def add_comment(comment, p):
+    u = UserProfile.objects.get(user=User.objects.get(username=comment["poster"]))
+    #p = Post.objects.get(slug = comment["post"])
+    c = Comment.objects.get_or_create(poster= u, post=p, time=comment["time"], content=comment["content"], likes=comment["likes"])[0]
     c.save()
     return c
 
