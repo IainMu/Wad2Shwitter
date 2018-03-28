a0bdea3c3e25fe5aec94e3198fd9344fa45d8b9a
KennyMenny <2263031d@student.gla.ac.uk>
2018-03-28 16:37:09 +0100

    stuff

---
+++ Schwitter_Project/static/Sidepanel.js
@@ -0,0 +1,10 @@
+function openNav() {
+		document.getElementById("Sidepanel").style.width = "250px";
+		document.getElementById("main").style.marginLeft = "250px";
+    	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+	}
+
+function closeNav() {
+		document.getElementById("Sidepanel").style.width = "0";
+		document.getElementById("main").style.marginLeft= "0";
+	}
