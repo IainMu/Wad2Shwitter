a3b812b1dc936b6cf0e575ade15040a3969c8f0a
Corey Ansley <2266643A@gla.ac.uk>
2018-03-28 16:38:30 +0100

    implemented tests

--- Schwitter_Project/Schwitter/forms.py
+++ Schwitter_Project/Schwitter/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from Schwitter.models import UserProfile,Post,Comment
 
 class UserForm(forms.ModelForm):
+    #form to create a new user
     password=forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
@@ -10,11 +11,13 @@ class UserForm(forms.ModelForm):
         fields = ('username','email','password','first_name', 'last_name',)
 
 class UserProfileForm(forms.ModelForm):
+    #form to upload picture to user profile
     class Meta:
         model=UserProfile
         fields=('picture',)
 
 class PostForm(forms.ModelForm):
+    #form to create new post
     poster=forms.ModelChoiceField(widget=forms.HiddenInput(),queryset=UserProfile,initial=None)
     class Meta:
         model=Post
@@ -22,6 +25,7 @@ class PostForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
+    #form to create new comment
     likes = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
     post = forms.ModelChoiceField(widget=forms.HiddenInput(),queryset=Post,initial=None)
     poster = forms.ModelChoiceField(widget=forms.HiddenInput(),queryset=UserProfile,initial=None)
